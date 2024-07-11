51330bfe64a17d95249514d8e6eff0e8cff1a022
devsheke <abhi@sheke.tech>
2024-07-11 15:35:49 +0530

    feat: update Account methods and reconfigure input reading

--- internal/models/input.go
+++ internal/models/input.go
@@ -14,27 +14,35 @@ import (
 // to display credit usage.
 const TimeFormat string = "Jan 02, 2006 3:04 PM MST"
 
+// ApolloAccount represents an apollo.io user in additon
+// to their respective scrape status indicators.
 type ApolloAccount struct {
 	Email         string `csv:"email"          json:"email"`
 	Password      string `csv:"password"       json:"password"`
+	List          string `csv:"list"           json:"list"`
+	URL           string `csv:"url"            json:"url"`
 	VpnConfig     string `csv:"vpn"            json:"vpn"`
 	Credits       int    `csv:"credits"        json:"credits"`
 	CreditRefresh *Time  `csv:"credit-refresh" json:"credit-refresh"`
 	Timeout       *Time  `csv:"timeout"        json:"timeout"`
 	Target        int    `csv:"target"         json:"target"`
 	Saved         int    `csv:"saved"          json:"saved"`
+	_done         bool   `csv:"-"`
 }
 
-func ReadInput(file string) ([]*ApolloAccount, error) {
+// ReadAccountsFile parses a CSV or JSON file containing apollo.io
+// account credentials and other data which corresponds to the
+// ApolloAccount type.
+func ReadAccountsFile(file string) ([]*ApolloAccount, error) {
 	ext := filepath.Ext(file)
 
 	var records []*ApolloAccount
 	var err error
 
 	switch ext {
-	case "json":
+	case ".json":
 		records, err = readFromJSON(file)
-	case "csv":
+	case ".csv":
 		records, err = readFromCSV(file)
 	default:
 		return nil, errors.New("unknown input file format")
@@ -56,46 +64,109 @@ func ReadInput(file string) ([]*ApolloAccount, error) {
 	return records, nil
 }
 
-func (a *ApolloAccount) Done() bool {
-	return a.Saved >= a.Target
+func readFromCSV(file string) ([]*ApolloAccount, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var records []*ApolloAccount
+	err = gocsv.UnmarshalFile(f, &records)
+
+	return records, err
 }
 
+func readFromJSON(file string) ([]*ApolloAccount, error) {
+	b, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var records []*ApolloAccount
+	err = json.Unmarshal(b, &records)
+
+	return records, err
+}
+
+// IsDone returns true if the ApolloAccount has no more new leads
+// to scrape.
+func (a *ApolloAccount) IsDone() bool {
+	return a.Saved >= a.Target || a._done
+}
+
+// IncSaved increases the amount of saved leads.
+func (a *ApolloAccount) IncSaved(amnt int) {
+	a.Saved += amnt
+}
+
+// Done updates the ApolloAccount scrape state to 'Completed'.
+func (a *ApolloAccount) Done() {
+	a._done = true
+}
+
+// IsTimedOut returns true if the ApolloAccount has hit the daily
+// limit of scraping new leads.
+func (a *ApolloAccount) IsTimedOut() bool {
+	if a.Timeout.ok {
+		return true
+	}
+
+	cond := time.Now().Before(a.Timeout.time)
+	if cond {
+		return true
+	}
+
+	a.Timeout.Reset()
+	return false
+}
+
+// SetTimeout sets a timeout lasting for the given duration
+func (a *ApolloAccount) SetTimeout(duration time.Duration) {
+	time := Time{true, time.Now().Add(duration)}
+	if a.Timeout == nil {
+		a.Timeout = &time
+	} else {
+		*a.Timeout = time
+	}
+}
+
+// UseCredits updates credit usage of an ApolloAccount.
+func (a *ApolloAccount) UseCredits(amnt int) {
+	a.Credits -= amnt
+}
+
+// CanScrape returns true if the ApolloAccount has enough credits to scrape leads.
+func (a *ApolloAccount) CanScrape() bool {
+	return a.Credits > 0
+}
+
+// Time is a representation of nullable library's time type.
 type Time struct {
 	ok   bool
 	time time.Time
 }
 
+func NewTime(time time.Time) *Time {
+	return &Time{true, time}
+}
+
+// Get returns the underlying time value
+func (t *Time) Get() time.Time {
+	return t.time
+}
+
+// Reset sets the time value to its zero equivalent.
 func (t *Time) Reset() {
 	t.ok = false
 	t.time = time.Time{}
 }
 
-func (t *Time) IsTimedOut() bool {
-	if !t.ok {
-		return false
-	}
-
-	cond := time.Now().Before(t.time)
-	if cond {
-		return cond
-	}
-	t.time = time.Time{}
-	return false
-}
-
-func (t *Time) SetTime(_t time.Time) {
-	t.ok = true
-	t.time = _t
-}
-
-func (t *Time) GetTime() (time.Time, bool) {
-	if t.ok {
-		return t.time, t.ok
-	}
-	return time.Time{}, t.ok
-}
-
 func (t *Time) marshal() (string, error) {
+	if !t.ok {
+		return "", nil
+	}
+
 	return t.time.Format(TimeFormat), nil
 }
 
@@ -130,29 +201,4 @@ func (t *Time) MarshalJSON() ([]byte, error) {
 
 func (t *Time) UnmarshalJSON(field []byte) error {
 	return t.unmarshal(string(field))
-}
-
-func readFromCSV(file string) ([]*ApolloAccount, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var records []*ApolloAccount
-	err = gocsv.UnmarshalFile(f, &records)
-
-	return records, err
-}
-
-func readFromJSON(file string) ([]*ApolloAccount, error) {
-	b, err := os.ReadFile(file)
-	if err != nil {
-		return nil, err
-	}
-
-	var records []*ApolloAccount
-	err = json.Unmarshal(b, &records)
-
-	return records, err
 }
