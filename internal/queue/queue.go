e0aef080ebb75d3da6926efb824008fa18a86bc7
devsheke <abhi@sheke.tech>
2024-07-11 15:45:20 +0530

    feat: create queue module for running scrape jobs

---
+++ internal/queue/queue.go
@@ -0,0 +1,220 @@
+package queue
+
+import (
+	"context"
+	"errors"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"github.com/shadowbizz/apollo-crawler/internal/actions"
+	"github.com/shadowbizz/apollo-crawler/internal/models"
+	"github.com/shadowbizz/apollo-crawler/internal/openvpn"
+)
+
+var ErrorEmptyQueue = errors.New("job queue is empty")
+
+// job represents a scrape task. It includes an apollo.io account
+// as well as an output destination for the scrape data.
+type job struct {
+	output  string
+	account *models.ApolloAccount
+}
+
+// Queue is a job queue which orchestrates and controls the execution
+// the scrape jobs alotted to each of the given apollo.io accounts.
+type Queue struct {
+	debug, fetchCredits, headless, saveProgress bool
+	limit, outputType                           int
+	jobs                                        []*job
+	outputDir, tab                              string
+	timeout                                     time.Duration
+	vpn                                         *openvpn.OpenVPN
+}
+
+// QueueOpt is a function which is used to configure the apollo.io scrape Queue.
+type QueueOpt func(q *Queue)
+
+// CSVOutput sets the output to CSV format.
+func CSVOutput() QueueOpt {
+	return func(q *Queue) {
+		q.outputType = models.CSVOutput
+	}
+}
+
+// Debug runs the Queue in debug mode.
+//
+// In this mode error screenshots are captured to better debug any problems faced
+// during the scrape
+func Debug(b bool) QueueOpt {
+	return func(q *Queue) {
+		q.debug = b
+	}
+}
+
+// FetchCredits specifies if the credits are to be fetched for each of the given initAccounts
+// before the scrape jobs are started.
+func FetchCredits(b bool) QueueOpt {
+	return func(q *Queue) {
+		q.fetchCredits = b
+	}
+}
+
+// SetTab is used to specify that each scraper scrapes detail from the given apollo tab.
+func SetTab(tab string) QueueOpt {
+	return func(q *Queue) {
+		switch tab {
+		case "new":
+			q.tab = actions.NetNewTab
+		case "saved":
+			q.tab = actions.SavedTab
+		case "total":
+			q.tab = actions.TotalTab
+		}
+	}
+}
+
+// Headless specifies whether Chrome should run in headless mode or not.
+func Headless(b bool) QueueOpt {
+	return func(q *Queue) {
+		q.headless = b
+	}
+}
+
+// JSONOutput sets the output to JSON format.
+func JSONOutput() QueueOpt {
+	return func(q *Queue) {
+		q.outputType = models.JSONOutput
+	}
+}
+
+// Dailyimit sets the maximum number of records that can be scraped per job, per day.
+// The default value for this option is 500.
+func Dailyimit(l int) QueueOpt {
+	return func(q *Queue) {
+		q.limit = l
+	}
+}
+
+// OutputDir specifies the directory which will be used to store all the relevant output files.
+func OutputDir(outputDir string) QueueOpt {
+	return func(q *Queue) {
+		q.outputDir = outputDir
+	}
+}
+
+// SaveProgress specifies if intermediary job progress should be stored.
+// The progress data includes the total saved leads and credit information.
+func SaveProgress(b bool) QueueOpt {
+	return func(q *Queue) {
+		q.saveProgress = b
+	}
+}
+
+// Timeout sets the maximum amount of time (in seconds) that can be spent on a chrome automation action.
+func Timeout(t time.Duration) QueueOpt {
+	return func(q *Queue) {
+		q.timeout = t
+	}
+}
+
+// VPN specifies that the Queue run with OpenVPN.
+func VPN(o *openvpn.OpenVPN) QueueOpt {
+	return func(q *Queue) {
+		q.vpn = o
+	}
+}
+
+func initAccounts(accounts *[]*models.ApolloAccount, vpn *openvpn.OpenVPN) {
+	for _, account := range *accounts {
+		if vpn != nil && account.VpnConfig != "" {
+			vpn.UpdateUsed(account.VpnConfig)
+		}
+	}
+
+	for _, account := range *accounts {
+		if vpn != nil {
+			if account.VpnConfig == "" {
+				for _, config := range vpn.Configs {
+					if !vpn.ConfigIsUsed(config) {
+						account.VpnConfig = config
+					}
+				}
+			}
+		}
+	}
+}
+
+// New instantiates a new Queue instance with the given apollo.io accounts as well as QueueOpts.
+func New(accounts []*models.ApolloAccount, opts ...QueueOpt) *Queue {
+	q := &Queue{
+		limit:      500,
+		timeout:    30 * time.Second,
+		outputDir:  "apollo-output",
+		outputType: models.CSVOutput,
+	}
+
+	for _, optFn := range opts {
+		optFn(q)
+	}
+
+	initAccounts(&accounts, q.vpn)
+
+	jobs := make([]*job, len(accounts))
+	for i, account := range accounts {
+		jobs[i] = &job{output: strings.ReplaceAll(account.Email, "@", "_"), account: account}
+	}
+
+	q.jobs = jobs
+
+	_ = os.MkdirAll(q.outputDir, 0755)
+
+	return q
+}
+
+// newChromeContext creates a new chromedp context with the specified configuration.
+func (q *Queue) newChromeContext() (context.Context, context.CancelFunc) {
+	opts := append(
+		chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", q.headless),
+		chromedp.WindowSize(1920, 1080),
+	)
+	alloc, _ := chromedp.NewExecAllocator(context.Background(), opts...)
+
+	return chromedp.NewContext(alloc)
+}
+
+func (q *Queue) front() (*job, error) {
+	if len(q.jobs) == 0 {
+		return nil, ErrorEmptyQueue
+	}
+
+	return q.jobs[0], nil
+}
+
+func (q *Queue) enqueueJob(job *job) {
+	q.jobs = append(q.jobs, job)
+}
+
+func (q *Queue) dequeueJob() (*job, error) {
+	if len(q.jobs) == 0 {
+		return nil, ErrorEmptyQueue
+	}
+
+	end := len(q.jobs) - 1
+	last := q.jobs[end]
+	q.jobs = q.jobs[:end]
+
+	return last, nil
+}
+
+func (q *Queue) sendToBack() error {
+	job, err := q.dequeueJob()
+	if err != nil {
+		return err
+	}
+	q.enqueueJob(job)
+
+	return nil
+}
---
+++ internal/queue/run.go
@@ -0,0 +1,211 @@
+package queue
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"github.com/rs/zerolog/log"
+	"github.com/shadowbizz/apollo-crawler/internal/actions"
+	"github.com/shadowbizz/apollo-crawler/internal/models"
+)
+
+var (
+	// ErrorTargetReached indicates that the scrape target set for an ApolloAccount
+	// has been successfully reached.
+	ErrorTargetReached = errors.New("scrape target has been reached")
+
+	// ErrorTargetReached indicates that the the daily limit for scraping an ApolloAccount
+	// has been reached.
+	ErrorDailyLimit = errors.New("scrape daily limit has been reachehd")
+)
+
+// fetchCredits fetches and updates the credits for each ApolloAccount present
+// in the queue.
+func (q *Queue) _fetchCredits() error {
+	for _, job := range q.jobs {
+		ctx, cancel := q.newChromeContext()
+
+		if err := actions.SignIn(ctx, job.account.Email, job.account.Password, q.timeout); err != nil {
+			cancel()
+			return err
+		}
+
+		current, refresh, err := actions.FetchCredits(ctx, q.timeout)
+		if err != nil {
+			cancel()
+			return err
+		}
+
+		job.account.Credits = current
+		job.account.CreditRefresh = models.NewTime(refresh)
+
+		cancel()
+	}
+
+	return nil
+}
+
+// _saveProgress saves the intermediary state of each ApolloAccount present
+// in the queue.
+func (q *Queue) _saveProgress() error {
+	accounts := make([]*models.ApolloAccount, len(q.jobs))
+	for i, job := range q.jobs {
+		accounts[i] = job.account
+	}
+
+	ext, err := models.ExtensionFromOutputType(q.outputType)
+	if err != nil {
+		return err
+	}
+
+	file := fmt.Sprintf("apollo-scrape-progress%s", ext)
+
+	return models.SaveRecordsToFile(accounts, filepath.Join(q.outputDir, file), q.outputType)
+}
+
+// scrapeJob runs a single isolated task of scraping leads from the url allocated
+// to each ApolloAccount until the daily limit has been reached or until completed.
+func (q *Queue) scrapeJob(job *job) error {
+	defer func() {
+		if q.vpn != nil {
+			if err := q.vpn.Stop(); err != nil {
+				log.Warn().Err(err).Msg("openvpn stop err")
+			}
+		}
+	}()
+
+	if q.vpn != nil {
+		if err := q.vpn.Start(job.account.VpnConfig); err != nil {
+			newConf, err := q.vpn.Backup()
+			if err != nil {
+				return err
+			}
+			job.account.VpnConfig = newConf
+		}
+	}
+
+	ctx, cancel := q.newChromeContext()
+	defer cancel()
+
+	if err := actions.SignIn(ctx, job.account.Email, job.account.Password, q.timeout); err != nil {
+		return err
+	}
+
+	if err := chromedp.Run(ctx, chromedp.Navigate(job.account.URL)); err != nil {
+		return err
+	}
+
+	if q.tab != "" {
+		if err := actions.SetTab(ctx, q.tab, q.timeout); err != nil {
+			return err
+		}
+	}
+
+	var lastErr error
+	var leads []*models.Lead
+	var retries int
+
+	for len(leads) <= q.limit {
+		if retries >= 10 {
+			return lastErr
+		}
+
+		_leads, err := actions.ScrapePage(ctx, q.timeout)
+		if err != nil {
+			retries++
+			lastErr = err
+			continue
+		}
+		leads = append(leads, _leads...)
+
+		err = models.SaveRecordsToFile(leads, filepath.Join(q.outputDir, job.output), q.outputType)
+		if err != nil {
+			retries++
+			lastErr = err
+			continue
+		}
+
+		job.account.IncSaved(len(leads))
+		job.account.UseCredits(len(leads))
+
+		// TODO: verbose credit usage tracking!
+
+		if q.saveProgress {
+			if err := q._saveProgress(); err != nil {
+				log.Warn().Err(err).Msg("failed to save scraper progress")
+			}
+		}
+
+		if job.account.IsDone() {
+			return ErrorTargetReached
+		}
+
+		if err := actions.NextPage(ctx, q.timeout); err != nil {
+			if errors.Is(err, actions.ErrorListEnd) {
+				return err
+			}
+			retries++
+			lastErr = err
+			continue
+		}
+	}
+
+	if len(leads) >= q.limit {
+		job.account.SetTimeout(24 * time.Hour)
+	}
+
+	return ErrorDailyLimit
+}
+
+// Run executes the queue of scrape jobs till completion (i.e., no more jobs are available).
+func (q *Queue) Run() error {
+	if q.fetchCredits {
+		if err := q._fetchCredits(); err != nil {
+			return err
+		}
+	}
+
+	for {
+		job, err := q.front()
+		if err != nil {
+			log.Info().Msg("finished all scraping jobs")
+			break
+		}
+
+		account := job.account
+
+		if account.IsDone() {
+			log.Info().Str("account", account.Email).Msg("scraper job complete")
+			_, _ = q.dequeueJob()
+			continue
+		}
+
+		if account.IsTimedOut() {
+			log.Debug().
+				Str("account", account.Email).
+				Time("till", account.Timeout.Get()).
+				Msg("skipping timed out job")
+
+			_ = q.sendToBack()
+			continue
+		}
+
+		err = q.scrapeJob(job)
+		if errors.Is(err, ErrorDailyLimit) {
+			log.Info().Str("account", account.Email).Msg("scraper job daily limit reached")
+			_ = q.sendToBack()
+		} else if errors.Is(err, ErrorTargetReached) || errors.Is(err, actions.ErrorListEnd) {
+			log.Info().Str("account", account.Email).Msg("scraper job complete")
+			_, _ = q.dequeueJob()
+		} else {
+			log.Error().Err(err).Str("account", account.Email).Msg("scraper job failed")
+			_ = q.sendToBack()
+		}
+		continue
+	}
+
+	return nil
+}
